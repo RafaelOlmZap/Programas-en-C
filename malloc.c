9117a4d9a2de095779482aefe09e57f407b31261
Rafael Antonio Olmos Zapata <170300077@ucaribe.edu.mx>
2019-12-03 01:18:00 -0500

    change the way the Malloc.c code is presented
    add a form of presentation of the Malloc.c code where the use of "sizeof" in the instruction is explained.

--- malloc.c
+++ malloc.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
-
+typedef struct Nodo{
+	int Num;
+}Nodo;
 void fn(){
 	printf("programa terminado\n");
 }
 
 void main(){
+	// Introducción a Malloc
+	// Con matrices para dar dimensión y declarando un tipo de dato...
+	int size, tops;
+	printf("Dame el valor de filas de tu matriz\n");
+	scanf("%d",&size);
+	printf("Dame el valor de columnas de tu matriz\n");
+	scanf("%d",&tops);
+	Nodo **E;
+	E=malloc(sizeof(Nodo)*size);
+	*E=malloc(sizeof(Nodo)*tops);
 	atexit(&fn);
 	// int size;
 	// scanf("%d",&size);
